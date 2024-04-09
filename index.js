8e041c0525f06db950c2a7ae0ed61a13f6a455a9
Felix Burkard <felix-burkard@web.de>
2024-04-09 02:20:26 +0200

    "Fixed" the algorithm
    Search now works as expected... it crashes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import { Page } from "./lib/parse.js";
+import { search } from "./lib/hiker.js";
 
-const page = new Page(new URL("https://de.wikipedia.org/wiki/Katzen"));
-await page.load();
-console.log(page.title);
-const references = page.getReferences();
-await Promise.all(references.map((x) => x.load()))
-references.forEach((x) => console.log(x.title));
+async function main() {
+  if (process.argv.length === 4) {
+    const startUrl = process.argv[2];
+    const targetUrl = process.argv[3];
+    console.log(`Start URL: ${startUrl}`);
+    console.log(`Target URL: ${targetUrl}`);
+    let result = await search(new URL(startUrl), new URL(targetUrl), 3);
+    if (result === undefined) {
+      console.log("No connection found");
+    } else {
+      result.printTrace();
+    }
+  } else {
+    console.log("Invalid usuage.\n Right usage: node index.js <start-url> <target-url>");
+    process.exit(1);
+  }
+}
+
+await main();
