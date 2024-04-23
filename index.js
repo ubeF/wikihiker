56dcac1ed749ee28f7851a769fc6ca2269852230
Mykhaylo <120499224+mykhayloserdyuk@users.noreply.github.com>
2024-04-23 16:11:54 +0200

    again adjust cli

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ program
   .argument("<startURL>", "starting URL")
   .argument("<targetURL>", "target URL")
   .option("-d, --depth <number>", "search depth", 3)
-  .option("-v, --verbose", "verbose output")
+  .option("-v, --verbosity <number>", "verbosity level", 1)
   .option(
     "-g, --graph database <path>",
     "database path",
@@ -22,55 +22,50 @@ program
   .option("-u, --username <username>", "username", null)
   .option("-p, --password <password>", "password", null)
   .action(async (startURL, targetURL, options) => {
-    try {
-      const depth = options.depth;
-      const graph = options.graph;
-      const username = options.username;
-      const password = options.password;
+    const depth = options.depth;
+    const graph = options.graph;
+    const username = options.username;
+    const password = options.password;
+    const verbosity = options.verbosity;
+    let database;
+    let res;
 
-      if (options.verbose) {
-        console.log("Start URL:", startURL);
-        console.log("Target URL:", targetURL);
-        console.log("Depth:", options.depth);
-      }
+    if ((password && !username) || (username && !password)) {
+      console.error("Username and password must be provided together\n ");
+      process.exit(0);
+    }
 
+    if (options.verbosity > 0) {
       if (graph) {
         console.log("Graph database:", graph);
-        console.log("Username:", username);
-        console.log("Password:", password);
-
-        try {
-          const database = new Database(graph, username, password);
-          await database.connect();
-        } catch (error) {
-          console.error("Failed to connect to database:", error.message);
-          process.exit(0);
-        }
       }
+      console.log("Start URL:", startURL);
+      console.log("Target URL:", targetURL);
+      console.log("Depth:", options.depth);
+    }
 
-      const res = await search(
-        startURL,
-        targetURL,
-        depth,
-        options.verbose,
-        database
-      );
-
-      if (res != undefined) {
-        res.printTrace();
-        await database.disconnect();
-      } else {
-        console.log("No path found");
-      }
+    try {
+      database = new Database(graph, username, password);
+      await database.connect();
     } catch (error) {
-      console.error("Invalid URL provided:", error.message);
+      console.error("Failed to connect to database:", error.message);
+      process.exit(0);
+    }
+
+    try {
+      res = await search(startURL, targetURL, depth, database, verbosity);
+    } catch (error) {
+      console.error("Failed to search:", error.message);
+      process.exit(0);
+    }
+
+    if (res != undefined) {
+      console.log("Path found:");
+      res.forEach((x) => console.log(x));
+      await database.disconnect();
+    } else {
+      console.log("No path found");
     }
   })
   .showHelpAfterError("(add --help or -h for additional information)")
   .parse();
-
-function conditionalLog(msg, cond) {
-  if (cond) {
-    console.log(msg);
-  }
-}
