b52d4ce08324d9fb66b930cc5c76522bba8dd89c
Mykhaylo <120499224+mykhayloserdyuk@users.noreply.github.com>
2024-04-26 14:55:43 +0200

    add subcommands search and clear. Implement clear database functionality

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const { search } = require("./lib/hiker.js");
 const { program } = require("commander");
 const { Database } = require("./lib/database.js");
 
+let database;
+
+async function connectDatabase(graph, username, password) {
+  if ((password && !username) || (username && !password)) {
+    console.error("Username and password must be provided together\n ");
+    process.exit(0);
+  }
+
+  try {
+    database = new Database(graph, username, password);
+    await database.connect();
+    return database;
+  } catch (error) {
+    console.error("Failed to connect to database:", error.message);
+    return undefined;
+  }
+}
+
 program
   .name("wikihiker")
   .description(
@@ -10,6 +28,7 @@ program
   .version("0.2.0");
 
 program
+  .command("search")
   .argument("<startURL>", "starting URL")
   .argument("<targetURL>", "target URL")
   .option("-d, --depth <number>", "search depth", 3)
@@ -27,13 +46,7 @@ program
     const username = options.username;
     const password = options.password;
     const verbosity = options.verbosity;
-    let database;
     let res;
-
-    if ((password && !username) || (username && !password)) {
-      console.error("Username and password must be provided together\n ");
-      process.exit(0);
-    }
 
     if (options.verbosity > 0) {
       if (graph) {
@@ -44,11 +57,9 @@ program
       console.log("Depth:", options.depth);
     }
 
-    try {
-      database = new Database(graph, username, password);
-      await database.connect();
-    } catch (error) {
-      console.error("Failed to connect to database:", error.message);
+    database = await connectDatabase(graph, username, password);
+
+    if (!database) {
       process.exit(0);
     }
 
@@ -67,5 +78,37 @@ program
       console.log("No path found");
     }
   })
-  .showHelpAfterError("(add --help or -h for additional information)")
-  .parse();
+  .showHelpAfterError("(add --help or -h for additional information)");
+
+program
+  .command("clear")
+  .option(
+    "-g, --graph database <path>",
+    "database path",
+    "neo4j://localhost:7687"
+  )
+  .option("-u, --username <username>", "username", null)
+  .option("-p, --password <password>", "password", null)
+  .action(async (options) => {
+    const username = options.username;
+    const password = options.password;
+    const graph = options.graph;
+
+    database = await connectDatabase(graph, username, password);
+
+    if (!database) {
+      process.exit(0);
+    }
+
+    try {
+      console.log("Deleted nodes:", await database.clearData());
+    } catch (error) {
+      console.error("Failed to clear database:", error.message);
+      process.exit(0);
+    }
+
+    await database.disconnect();
+  })
+  .showHelpAfterError("(add --help or -h for additional information)");
+
+program.parse(process.argv);
